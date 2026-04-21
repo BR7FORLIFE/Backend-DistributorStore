db0b1463fc7700ddc8e0742a0ab1d5d1bcddeec1
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-21 16:00:32 -0500

    feat: Add ProductController for product management with CRUD operations and authorization

--- src/main/java/com/tecno_comfenalco/pa/infrastructure/auth/controller/AuthenticationController.java
+++ src/main/java/com/tecno_comfenalco/pa/infrastructure/auth/controller/AuthenticationController.java
@@ -2,8 +2,6 @@ package com.tecno_comfenalco.pa.infrastructure.auth.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
