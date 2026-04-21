b9cca08175622896708713b6ef50fd1410eeedcf
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-21 15:58:35 -0500

    feat: Add custom exception classes and global advice for catalog and product handling

---
+++ src/main/java/com/tecno_comfenalco/pa/infrastructure/ZGlobalAdviceExceptions/CatalogGlobalAdviceException.java
@@ -0,0 +1,31 @@
+package com.tecno_comfenalco.pa.infrastructure.ZGlobalAdviceExceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tecno_comfenalco.pa.application.catalog.exceptions.CatalogExistsException;
+import com.tecno_comfenalco.pa.application.catalog.exceptions.CategoryExistsException;
+import com.tecno_comfenalco.pa.shared.utils.helper.ApiError;
+import com.tecno_comfenalco.pa.shared.utils.helper.StaticError;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class CatalogGlobalAdviceException {
+
+    @ExceptionHandler(CatalogExistsException.class)
+    public ResponseEntity<ApiError> handleCatalogExistsException(
+            CatalogExistsException ex,
+            HttpServletRequest request) {
+        return StaticError.buildError(HttpStatus.NOT_ACCEPTABLE, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(CategoryExistsException.class)
+    public ResponseEntity<ApiError> handleCategoryExistsException(
+            CategoryExistsException ex,
+            HttpServletRequest request) {
+        return StaticError.buildError(HttpStatus.NOT_ACCEPTABLE, ex.getMessage(), request);
+    }
+}
