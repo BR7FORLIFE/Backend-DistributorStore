e0951abbf502a1038db089991a48203868a554a5
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-23 16:13:12 -0500

    feat: Refactor UserMapper to remove MapStruct dependency and implement static mapping methods

--- src/main/java/com/tecno_comfenalco/pa/infrastructure/auth/mapper/UserMapper.java
+++ src/main/java/com/tecno_comfenalco/pa/infrastructure/auth/mapper/UserMapper.java
@@ -1,12 +1,23 @@
 package com.tecno_comfenalco.pa.infrastructure.auth.mapper;
 
-import org.mapstruct.Mapper;
-
 import com.tecno_comfenalco.pa.domain.auth.models.UserModel;
 import com.tecno_comfenalco.pa.infrastructure.auth.entity.UserDocument;
-import com.tecno_comfenalco.pa.shared.mapper.EntityMapper;
 
-@Mapper(componentModel = "spring")
-public interface UserMapper extends EntityMapper<UserModel, UserDocument> {
+public class UserMapper {
+    public static UserModel toDomain(UserDocument userDocument) {
+        UserModel userModel = UserModel.createNew(userDocument.getId(), userDocument.getUsername(),
+                userDocument.getPassword(), userDocument.getRoles(), userDocument.isEnabled());
+        return userModel;
+    }
 
+    public static UserDocument toEntity(UserModel userModel) {
+        UserDocument userDocument = new UserDocument();
+        userDocument.setId(userModel.getId());
+        userDocument.setUsername(userModel.getUsername());
+        userDocument.setPassword(userModel.getPassword());
+        userDocument.setRoles(userModel.getRoles());
+        userDocument.setEnabled(userModel.isEnabled());
+
+        return userDocument;
+    }
 }
