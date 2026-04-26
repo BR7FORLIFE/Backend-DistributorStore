e0aeb3fbee6c873bcb4d250efa5f1f5f933d4634
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-26 17:19:16 -0500

    feat: Add custom authentication and delivery exception handling

--- src/main/java/com/tecno_comfenalco/pa/infrastructure/ZGlobalAdviceExceptions/AuthGlobalAdviceException.java
+++ src/main/java/com/tecno_comfenalco/pa/infrastructure/ZGlobalAdviceExceptions/AuthGlobalAdviceException.java
@@ -2,10 +2,12 @@ package com.tecno_comfenalco.pa.infrastructure.ZGlobalAdviceExceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.tecno_comfenalco.pa.application.auth.Exceptions.AuthenticationException;
+import com.tecno_comfenalco.pa.application.auth.Exceptions.BadCredentialException;
+import com.tecno_comfenalco.pa.application.auth.Exceptions.UserNotEnabledException;
 import com.tecno_comfenalco.pa.application.auth.Exceptions.UserNotFoundException;
 import com.tecno_comfenalco.pa.shared.utils.helper.ApiError;
 import com.tecno_comfenalco.pa.shared.utils.helper.StaticError;
@@ -15,10 +17,31 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class AuthGlobalAdviceException {
 
-    @ExceptionHandler(UsernameNotFoundException.class)
+    @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ApiError> handleUserNotFoundException(
             UserNotFoundException ex,
             HttpServletRequest request) {
         return StaticError.buildError(HttpStatus.NOT_FOUND, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(UserNotEnabledException.class)
+    public ResponseEntity<ApiError> handleUserNotEnabledException(
+            UserNotEnabledException ex,
+            HttpServletRequest request) {
+        return StaticError.buildError(HttpStatus.UNAUTHORIZED, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(BadCredentialException.class)
+    public ResponseEntity<ApiError> handleBadCredentialException(
+            BadCredentialException ex,
+            HttpServletRequest request) {
+        return StaticError.buildError(HttpStatus.UNAUTHORIZED, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(
+            AuthenticationException ex,
+            HttpServletRequest request) {
+        return StaticError.buildError(HttpStatus.UNAUTHORIZED, ex.getMessage(), request);
     }
 }
