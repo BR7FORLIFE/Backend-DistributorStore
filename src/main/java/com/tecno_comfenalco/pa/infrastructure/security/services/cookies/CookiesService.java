758c4cdb315e55969f430be9d591e2e19f6be936
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-30 11:13:18 -0500

    feat(cookies): enhance cookie management with secure flag and refactor cookie creation

--- src/main/java/com/tecno_comfenalco/pa/infrastructure/security/services/cookies/CookiesService.java
+++ src/main/java/com/tecno_comfenalco/pa/infrastructure/security/services/cookies/CookiesService.java
@@ -15,6 +15,9 @@ public class CookiesService {
     @Value("${jwt.expiration-ms}")
     private Long expirationMs;
 
+    @Value("${cookie.secure}")
+    private boolean isSecure;
+
     private final JwtService jwtService;
 
     public CookiesService(JwtService jwtService) {
@@ -22,30 +25,33 @@ public class CookiesService {
     }
 
     public void sendCookie(HttpServletResponse response, LoginRequestDto request) {
-        long defaultExpiration = 60 * 60 * 1000L; // 1 hora por defecto
-        long configuredExpiration = expirationMs != null ? expirationMs.longValue() : defaultExpiration;
+        long defaultExpiration = 3600000L; 
+        long configuredExpiration = expirationMs != null ? expirationMs : defaultExpiration;
         long expirationTime = request.rememberMe() ? 7L * 24 * 60 * 60 * 1000 : configuredExpiration;
 
         String token = jwtService.encode(request.username(), expirationTime);
 
-        Cookie cookie = new Cookie("jwt", token);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false);
-        cookie.setPath("/");
-        cookie.setDomain("localhost");
-        cookie.setMaxAge((int) (expirationTime / 1000));
-        cookie.setAttribute("SameSite", "Lax");
+        Cookie cookie = createCookie("jwt", token, (int) (expirationTime / 1000));
         response.addCookie(cookie);
     }
 
     public void deleteCookie(HttpServletResponse response) {
-        Cookie cookie = new Cookie("jwt", "");
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false);
-        cookie.setPath("/");
-        cookie.setDomain("localhost");
-        cookie.setMaxAge(0);
-        cookie.setAttribute("SameSite", "Lax");
+        Cookie cookie = createCookie("jwt", "", 0);
         response.addCookie(cookie);
+    }
+
+    private Cookie createCookie(String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+
+        cookie.setSecure(isSecure);
+        if (isSecure) {
+            cookie.setAttribute("SameSite", "None");
+        } else {
+            cookie.setAttribute("SameSite", "Lax");
+        }
+        return cookie;
     }
 }
