3ecaaa6178519f8657fb04267d509b2891a931aa
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-05-08 10:40:41 -0500

    feat(inventory): Implement inventory management features
    - Added endpoints for creating, updating, retrieving, and deleting inventory items.
    - Created command and response objects for inventory operations.
    - Implemented InventoryUseCase for business logic handling.
    - Developed InventoryController for handling HTTP requests.
    - Introduced InventoryRepository for data persistence with MongoDB.
    - Added exception handling for inventory-related errors.
    - Created DTOs for request and response data transfer.
    - Implemented inventory model and mapper for domain logic.

---
+++ src/main/java/com/tecno_comfenalco/pa/application/inventory/ports/IInventoryRepositoryPort.java
@@ -0,0 +1,18 @@
+package com.tecno_comfenalco.pa.application.inventory.ports;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import com.tecno_comfenalco.pa.domain.inventory.models.InventoryModel;
+import com.tecno_comfenalco.pa.shared.utils.http.PagedResult;
+
+public interface IInventoryRepositoryPort {
+    InventoryModel save(InventoryModel model);
+
+    Optional<InventoryModel> findByIdAndDistributorId(UUID inventoryId, UUID distributorId);
+
+    PagedResult<InventoryModel> findAllPaged(UUID distributorId, Integer page, Integer size, String sortBy,
+            String direction);
+
+    void deleteByIdAndDistributorId(UUID inventoryId, UUID distributorId);
+}
---
+++ src/main/java/com/tecno_comfenalco/pa/infrastructure/inventory/entity/InventoryDocument.java
@@ -0,0 +1,23 @@
+package com.tecno_comfenalco.pa.infrastructure.inventory.entity;
+
+import java.util.UUID;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "inventory")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InventoryDocument {
+    private UUID id;
+    private UUID distributorId;
+    private UUID productId;
+    private UUID warehouseId;
+    private Long quantity;
+}
