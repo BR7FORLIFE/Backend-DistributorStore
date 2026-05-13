04c7917e0c84edfdfb80c7fe4b56b85b502d322e
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-05-13 14:18:47 -0500

    feat: Remove UserCommandLineRunner and ensure DataSeederController is active for non-prod Mongo profiles

--- src/main/java/com/tecno_comfenalco/pa/config/DataSeederController.java
+++ src/main/java/com/tecno_comfenalco/pa/config/DataSeederController.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-//@Profile("!mongo-prod")
+@Profile("!mongo-prod")
 @PreAuthorize("hasRole('ADMIN')")
 @RestController
 @RequestMapping("/seeder")
